d3130acba97612c18675cc9bbdc7dbf07706ba83
Himanshu <himanshusingh2087@gmail.com>
2025-07-10 19:21:58 +0530

    "cors config"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 //https://karigar-5vd9jayi8-himanshubabbers-projects.vercel.app
+//https://karigarbackend.vercel.app/
 export default defineConfig({
   plugins: [react()],
   server: {
