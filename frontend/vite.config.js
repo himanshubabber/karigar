2eb4574e469b9e4e02c6df848163ed267af3d35c
Himanshu <himanshusingh2087@gmail.com>
2025-07-10 16:21:29 +0530

    "backend_checking"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    // https://karigar-5vd9jayi8-himanshubabbers-projects.vercel.app/
     proxy: {
-      "/api":"https://karigar-5vd9jayi8-himanshubabbers-projects.vercel.app",
-       
+      '/api': {
+        target: 'https://karigar-5vd9jayi8-himanshubabbers-projects.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
