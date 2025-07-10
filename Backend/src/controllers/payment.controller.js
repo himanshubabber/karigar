49137ed7e82a6c3daea7c2e495d6d6c47e4f3675
Himanshu <himanshusingh2087@gmail.com>
2025-07-10 16:15:13 +0530

    "backend_request  checking"

--- Backend/src/controllers/payment.controller.js
+++ Backend/src/controllers/payment.controller.js
@@ -254,3 +254,5 @@ export {
   createOrderForWorker,
   verifyPaymentForWorker,
 };
+
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     // 
     proxy: {
-      "/api":"http://localhost:8000",
+      "/api":"https://karigar-oryzjbqsq-himanshubabbers-projects.vercel.app",
        
     },
   },
