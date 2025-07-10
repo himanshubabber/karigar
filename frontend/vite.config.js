a1baa7bb98958e3411ce29b62f6a1a9fd7a3d260
Himanshu <himanshusingh2087@gmail.com>
2025-07-10 17:08:26 +0530

    "created vercel.json"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+//https://karigar-5vd9jayi8-himanshubabbers-projects.vercel.app
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://karigar-5vd9jayi8-himanshubabbers-projects.vercel.app',
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
