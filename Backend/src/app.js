5238554b203d1274739e94d7b12f50004cd228dd
Himanshu <himanshusingh2087@gmail.com>
2025-07-10 18:21:01 +0530

    " cors reconfigured"

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,6 +23,8 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
