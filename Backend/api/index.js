94f203c4deb19a74b83c0991679eeaab1d363e02
Himanshu <himanshusingh2087@gmail.com>
2025-07-16 23:27:40 +0530

    "cors added in index.js"

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -37,4 +37,35 @@ app.get("/api/ping", (req, res) => {
   res.json({ ping: "pong", time: new Date() });
 });
 
+app.use(cors({
+  origin:process.env.CORS_ORIGIN  ||  "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}))
+
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import
+import customerRouter from "./src/routes/customer.route.js"
+import workerRouter from "./src/routes/worker.route.js"
+import serviceRequestRouter from "./src/routes/serviceRequest.route.js"
+import paymentRouter from "./src/routes/payment.route.js"
+
+
+
+
+
+//routes declaration
+app.use("/api/v1/customer", customerRouter)
+app.use("/api/v1/worker", workerRouter)
+app.use("/api/v1/payment", paymentRouter)
+app.use("/api/v1/serviceRequest", serviceRequestRouter);
+
+
+
 export default app;
