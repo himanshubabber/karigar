8c41da5b31b96650e0ff259e07602ba0f34d0dc5
Himanshu <himanshusingh2087@gmail.com>
2025-07-16 19:07:01 +0530

    "changed index.js for backend deployment fix"

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "../src/db/index.js";
-import {app} from './app.js'
+import app from './app.js'
 import serverlessExpress from '@vendia/serverless-express';
 dotenv.config({
     path: './.env'
