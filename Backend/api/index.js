aea201ca86ea2aedf7abe01a0d93a41c70d5353d
Himanshu <himanshusingh2087@gmail.com>
2025-07-16 19:14:11 +0530

    "change in index.js to remove serverless connection"

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "../src/db/index.js";
 import app from '../app.js'
-import serverlessExpress from '@vendia/serverless-express';
 dotenv.config({
     path: './.env'
 })
@@ -18,4 +17,3 @@ connectDB()
     console.log("MONGO db connection failed !!! ", err);
 })
 
-serverlessExpress({ app })
