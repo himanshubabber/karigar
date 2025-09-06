26f5a6da817edb03bb9ec7314d6bc78955382e3b
Himanshu <himanshusingh2087@gmail.com>
2025-09-06 18:40:46 +0530

    "google login issue resolved of button style"

--- frontend/src/pages/customer/Signin_customer.jsx
+++ frontend/src/pages/customer/Signin_customer.jsx
@@ -138,7 +138,7 @@ const Signin_customer = () => {
       onError={() => console.log("Google login failed")}
        theme="filled_blue"
       size="large"
-      width="28rem" // max width allowed
+      width={448} // max width allowed
     ></GoogleLogin>
  
 
